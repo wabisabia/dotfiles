5be9e4a9c2b26bcb715ce714998996ad8b8aa931
wabisabia <bkkchris.dunn@outlook.com>
2023-12-18 16:22:46 +0000

    Remove nvm.fish and switch to eza

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-		nvm use latest
-
-		alias ls exa
+		alias ls eza
 
     abbr -a fo "nvim ~/.config/fish/config.fish && source ~/.config/fish/config.fish"
 
