29cc62da55075c6147b1d9fb91de3c5fc228c3d1
wabisabia <bkkchris.dunn@outlook.com>
2023-12-18 13:45:47 +0000

    More abbrs

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,11 @@ if status is-interactive
 		alias ls exa
 		abbr -a gs "git status"
 		abbr -a gd "git diff"
+		abbr -a gds "git diff --staged"
 		abbr -a ga "git add"
 		abbr -a gu "git restore --staged"
+    abbr -a gc "git commit"
+    abbr -a gcm "git commit -m"
     abbr -a fo "nvim ~/.config/fish/config.fish && source ~/.config/fish/config.fish"
 		fish_vi_key_bindings
 end
