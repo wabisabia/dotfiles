9e0c23340da2db45a08016fe83670c89212a86a0
wabisabia <bkkchris.dunn@outlook.com>
2023-12-18 13:41:27 +0000

    Fish improvements

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 		nvm use latest
 		alias ls exa
+		abbr -a gs "git status"
+		abbr -a gd "git diff"
+		abbr -a ga "git add"
+		abbr -a gu "git restore --staged"
+    abbr -a fo "nvim ~/.config/fish/config.fish && source ~/.config/fish/config.fish"
 		fish_vi_key_bindings
-		alias cswk="cd ~/dev/uni/high-performance-computing/cswk/cfd/"
 end
 
 starship init fish | source
+
