3d49215f8a092fb1a518884139cd15b61e51461e
wabisabia <bkkchris.dunn@outlook.com>
2024-01-09 11:52:15 +0000

    Fix inconsistent spacing

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-		alias ls eza
+    alias ls eza
 
     abbr -a fo "nvim ~/.config/fish/config.fish && source ~/.config/fish/config.fish"
 
     abbr -a gf "git fetch"
     abbr -a gpl "git pull"
-		abbr -a gs "git status"
+    abbr -a gs "git status"
     abbr -a gl "git log"
     abbr -a gln "git log -n"
-		abbr -a gd "git diff"
-		abbr -a gds "git diff --staged"
-		abbr -a ga "git add"
-		abbr -a gu "git restore --staged"
+    abbr -a gd "git diff"
+    abbr -a gds "git diff --staged"
+    abbr -a ga "git add"
+    abbr -a gu "git restore --staged"
     abbr -a gc "git commit"
     abbr -a gcm "git commit -m"
     abbr -a gca "git commit --amend"
@@ -20,7 +20,7 @@ if status is-interactive
 
     abbr -a e "nvim"
 
-		fish_vi_key_bindings
+    fish_vi_key_bindings
     fzf_configure_bindings --directory=\cf
 end
 
