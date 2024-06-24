9a2a9dc8e07935e86d9c9655df14a7a2fd017b22
chri5d <christian.d@gardeniatech.com>
2024-06-24 14:06:31 +0100

    Personalise mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,14 +1,29 @@
 require "nvchad.mappings"
 
-local map = vim.keymap.set
-
-map("n", "/", "/\\v", { desc = "search regex" })
-
 local nomap = vim.keymap.del
 
 nomap("i", "<C-h>")
 nomap("i", "<C-l>")
+
 nomap("n", "<C-s>")
 nomap("n", "<C-c>")
-nomap("n", "<leader>n")
+
+nomap("n", "<Tab>")
+nomap("n", "<S-Tab>")
+
 nomap("n", "<leader>rn")
+nomap("n", "<leader>n")
+
+local map = vim.keymap.set
+
+map("n", "<C-t>n", function()
+  require("nvchad.tabufline").next()
+end, { desc = "buffer goto next" })
+
+map("n", "<C-t>p", function()
+  require("nvchad.tabufline").prev()
+end, { desc = "buffer goto prev" })
+
+map("n", "/", "/\\v", { desc = "search regex" })
+
+map("n", "L", vim.diagnostic.open_float, { desc = "diagnostics show in floating window" })
