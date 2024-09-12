1bd69ba71991b1772b4fc0ca25cf1168ca23ab62
Christian Dunn <bkkchris.dunn@outlook.com>
2024-09-12 20:42:16 +0100

    refactor(git): use git config aliases, not fish ones

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,31 +4,6 @@ end
 
 fish_vi_key_bindings
 
-# General `git` cmds
-abbr -a gf "git fetch"
-abbr -a gpl "git pull"
-abbr -a gs "git status"
-abbr -a gl "git log"
-abbr -a gln "git log -n"
-abbr -a gd "git diff"
-abbr -a gds "git diff --staged"
-abbr -a ga "git add"
-abbr -a gu "git restore --staged"
-abbr -a gr "git restore"
-abbr -a gc "git commit"
-abbr -a gcm "git commit -m"
-abbr -a gca "git commit --amend"
-abbr -a gps "git push"
-
-# Branch cmds
-abbr -a gba "git branch"
-abbr -a gbu "git branch --set-upstream-to"
-abbr -a gbp "git remote update origin --prune"
-
-# Worktree cmds
-abbr -a gwp "git worktree prune -v"
-abbr -a gwr "git worktree remove"
-
 if test (uname) = "Darwin"
   set -gx HOMEBREW_PREFIX "/opt/homebrew";
   set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
@@ -48,6 +23,10 @@ end
 
 if command -sq nvim 
   alias e nvim
+end
+
+if command -sq git
+  alias g git
 end
 
 if functions -q fzf_configure_bindings
