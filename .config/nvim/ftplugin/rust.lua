f3b339bf0298e2efd0d563cd831defec12d85db7
Christian Dunn <christian.d@gardeniatech.com>
2024-10-03 13:03:39 +0100

    feat(nvim, rust): add pedantic clippy lint keymap

--- .config/nvim/ftplugin/rust.lua
+++ .config/nvim/ftplugin/rust.lua
@@ -1,11 +1,14 @@
 vim.keymap.set("n", "<leader>ac", "<cmd>Cargo check<cr>")
+vim.keymap.set("n", "<leader>al", "<cmd>Cargo clippy -- -D clippy::pedantic<cr>")
 vim.keymap.set("n", "<leader>ab", "<cmd>Cargo build<cr>")
 vim.keymap.set("n", "<leader>at", "<cmd>Cargo test<cr>")
 vim.keymap.set("n", "<leader>ad", "<cmd>Cargo doc --open<cr>")
+vim.keymap.set("n", "<leader>ar", "<cmd>Cargo run<cr>")
 
 vim.keymap.set("n", "<leader>aC", ":Cargo check ")
 vim.keymap.set("n", "<leader>aB", ":Cargo build ")
 vim.keymap.set("n", "<leader>aT", ":Cargo test ")
 vim.keymap.set("n", "<leader>aD", ":Cargo doc ")
+vim.keymap.set("n", "<leader>aR", ":Cargo run")
 
 vim.keymap.set("n", "<leader>a<C-d>", "<cmd>Cargo doc<cr>")
