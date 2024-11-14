6bcb0f7aed97372b42beb99b86ec8187429d738d
Christian Dunn <christian.d@gardeniatech.com>
2024-11-14 22:42:57 +0000

    feat(nvim): add `rainbow_csv.nvim`

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -121,6 +121,26 @@ return {
 
   -- We can edit structured text??
   {
+    "cameron-wags/rainbow_csv.nvim",
+    config = true,
+    ft = {
+      "csv",
+      "tsv",
+      "csv_semicolon",
+      "csv_whitespace",
+      "csv_pipe",
+      "rfc_csv",
+      "rfc_semicolon",
+    },
+    cmd = {
+      "RainbowDelim",
+      "RainbowDelimSimple",
+      "RainbowDelimQuoted",
+      "RainbowMultiDelim",
+    },
+  },
+
+  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
