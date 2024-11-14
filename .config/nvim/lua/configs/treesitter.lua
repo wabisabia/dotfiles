eb94dbeca51a5e0d95285f7f99e97e0999c3285a
Christian Dunn <christian.d@gardeniatech.com>
2024-11-14 22:44:32 +0000

    refactor(nvim)!: make 'peek' keymaps mnemonic

--- .config/nvim/lua/configs/treesitter.lua
+++ .config/nvim/lua/configs/treesitter.lua
@@ -45,8 +45,8 @@ configs.setup {
       border = "none",
       floating_preview_opts = {},
       peek_definition_code = {
-        ["<leader>df"] = "@function.outer",
-        ["<leader>dF"] = "@class.outer",
+        ["<leader>paf"] = "@function.outer",
+        ["<leader>pac"] = "@class.outer",
       },
     },
   },
