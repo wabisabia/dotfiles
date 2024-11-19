f2e18c87f66058a946984f221fbc25702886c919
Christian Dunn <christian.d@gardeniatech.com>
2024-11-19 18:59:54 +0000

    fix(nvim, python): change clobbering check keymaps

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -4,11 +4,11 @@ util.map("n", "<leader>r", function()
   util.run_in_split("python " .. vim.fn.expand "%")
 end, "run current file")
 
-util.map("n", "<leader>l", function()
+util.map("n", "<leader>c", function()
   util.run_in_split { "ruff", "check" }
 end, "lint project")
 
-util.map("n", "<leader>L", function()
+util.map("n", "<leader>C", function()
   util.run_in_split { "ruff", "check", "--fix" }
   vim.cmd ":e"
 end, "fix autofixable lints")
