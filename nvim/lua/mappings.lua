d6446d606664526a8fd1d974063220e66edf3cd3
Christian Dunn <chrisd@wabisabia.com>
2024-11-29 17:47:33 +0000

    feat(nvim, tree): add/update file explorer toggle keymaps

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -15,7 +15,8 @@ util.map("n", "<leader>k", "<cmd>BufferLineCyclePrev<cr>", "goto prev buffer")
 
 -- Show
 util.map("n", "L", vim.diagnostic.open_float, "show diagnostics at cursor")
-util.map("n", "<C-n>", "<cmd>NvimTreeToggle<cr>", "show file explorer")
+util.map("n", "<C-n>", vim.cmd.NvimTreeFindFileToggle, "toggle file explorer at current file")
+util.map("n", "<C-S-n>", vim.cmd.NvimTreeToggle, "toggle file explorer")
 
 -- List
 util.map("n", "<leader>lt", "<cmd>Trouble todo toggle auto_jump=1 auto_close=1<cr>", "list todos")
