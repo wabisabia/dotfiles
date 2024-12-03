caf18ff847ee0a163be4fb34afc17f5e5fd33d17
Christian Dunn <christian.d@gardeniatech.com>
2024-12-03 15:51:23 +0000

    Add better AWS CLI completions

--- fish/config.fish
+++ fish/config.fish
@@ -38,6 +38,11 @@ if command -sq jj
   set -gx JJ_CONFIG ~/.config/jj/config.toml
 end
 
+if command -sq aws
+  # Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
+  complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+end
+
 if command -sq terraform
   alias tf terraform
 end
