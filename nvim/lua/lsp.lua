7b9fe302d1d43d8fbda6acf61b06841280e68357
wabisabia <chrisd@wabisabia.com>
2025-03-16 22:37:16 +0000

    Add `fzf-lua` configuration

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -4,18 +4,15 @@ M = {}
 
 ---@type fun(bufnr: integer)
 M.set_common_lsp_keymaps = function(bufnr)
+  local fzf = require("fzf-lua")
   -- Navigation
-  util.map("n", "gd", vim.lsp.buf.definition, "definitions in project", bufnr)
+  util.map("n", "gd", vim.lsp.buf.definition, "Go to definition", bufnr)
   util.map("n", "gD", vim.lsp.buf.declaration, "Go to declaration", bufnr)
   util.map("n", "gi", vim.lsp.buf.implementation, "Go to implementation", bufnr)
-  util.map("n", "gr", "<cmd>Trouble close<cr><cmd>Trouble lsp_references first<cr>", "references in project", bufnr)
-  util.map(
-    "n",
-    "gR",
-    "<cmd>Trouble close<cr><cmd>Trouble lsp_references_buffer first<cr>",
-    "references in buffer",
-    bufnr
-  )
+  util.map("n", "gr", fzf.lsp_references, "Go to references", bufnr)
+  util.map("n", "gt", fzf.lsp_live_workspace_symbols, "Go to symbol", bufnr)
+  util.map("n", "ge", fzf.diagnostics_document, "Go to document diagnostic", bufnr)
+  util.map("n", "g<c-e>", fzf.diagnostics_workspace, "Go to workspace diagnostic", bufnr)
 
   -- Rename/reword
   util.map("n", "<leader>rn", function()
@@ -25,16 +22,7 @@ M.set_common_lsp_keymaps = function(bufnr)
     util.input(vim.lsp.buf.rename, { prompt = "New name: ", default = vim.fn.expand "<cword>" })
   end, "Reword", bufnr)
 
-  -- Workspace operations
-  util.map("n", "<leader>wa", function()
-    util.input(vim.lsp.buf.add_workspace_folder, { prompt = "New workspace folder: " })
-  end, "Add workspace folder", bufnr)
-  util.map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, "Remove workspace folder", bufnr)
-  util.map("n", "<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, "List workspace folders", bufnr)
-
-  util.map({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, "List code actions", bufnr)
+  util.map({ "n", "v" }, "<leader>a", fzf.lsp_code_actions, "List code actions", bufnr)
 end
 
 return M
--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -5,9 +5,23 @@ return {
   dependencies = { "echasnovski/mini.icons" },
   config = function()
     local fzf = require("fzf-lua")
+    fzf.setup({
+      files = {
+        git_icons = true,
+        hidden = false,
+      },
+      lsp = {
+        symbols = {
+          symbol_style = 2,
+          symbol_fmt = function(s) return s end,
+        },
+      },
+    })
     local util = require("util")
-    util.map("n", "<leader>f", fzf.files, "search files")
+    util.map("n", "<leader>f", function() fzf.files({ git_icons = true }) end, "search files")
+    util.map("n", "<leader>F", function() fzf.files({ hidden = true }) end, "search files (include hidden)")
     util.map("n", "<leader>w", fzf.live_grep, "search file contents")
     util.map("n", "<leader>o", fzf.buffers, "search buffers")
+    util.map("n", "<leader>h", fzf.helptags, "search helptags")
   end
 }
