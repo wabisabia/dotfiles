f294c303bf86c9a08ec7871ecd67c7c8eeb03513
Christian Dunn <chrisd@wabisabia.com>
2025-04-02 22:12:51 +0100

    Change conflicting keymap

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -8,6 +8,6 @@ return {
     local util = require("util")
     util.map("n", "<leader>f", fzf.files, "search files")
     util.map("n", "<leader>w", fzf.live_grep, "search file contents")
-    util.map("n", "<leader>b", fzf.buffers, "search buffers")
+    util.map("n", "<leader>o", fzf.buffers, "search buffers")
   end
 }
