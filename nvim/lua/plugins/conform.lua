514a6a92eaa44ba9d30865f4a88ca4f3adc5dd42
Christian Dunn <christian.d@gardeniatech.com>
2025-04-29 12:48:25 +0100

    Disable formatting for `yaml` fts and add 'toggle formatting' commands

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -22,10 +22,32 @@ return {
         },
       },
 
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "prefer",
-      },
+      format_on_save = function(bufnr)
+        if vim.o.ft == "yaml" then
+          return
+        end
+        if vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return {
+          timeout_ms = 500,
+          lsp_format = "prefer",
+        }
+      end
+    })
+
+    vim.api.nvim_create_user_command("FormatDisable", function()
+      vim.b.disable_autoformat = true
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = false
+    })
+
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+      bang = false
     })
 
     util.map("n", "<leader>af", function()
