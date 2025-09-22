075ac9c6e99a20f42c0562becb65604b10d9862c
chrisd <chrisd@wabisabia.com>
2025-09-22 09:56:42 +0100

    Remove unmaintained plugin :(

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -1,8 +1,11 @@
 ---@module "lazy"
 ---@type LazyPluginSpec
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  version = "*",
+  'MeanderingProgrammer/render-markdown.nvim',
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },   -- if you use the mini.nvim suite
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
   opts = {},
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 }
