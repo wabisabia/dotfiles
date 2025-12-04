87c80d3ce5945775e50fa3b83ea4e8837b58f077
Christian Dunn <chris@thema.com>
2025-12-04 10:12:36 +0000

    feat(nvim): upgrade rustaceanvim

--- nvim/lua/plugins/rustaceanvim.lua
+++ nvim/lua/plugins/rustaceanvim.lua
@@ -2,8 +2,8 @@
 ---@type LazyPluginSpec
 return {
   "mrcjkb/rustaceanvim",
-  version = "^6", -- Recommended
-  lazy = false,   -- This plugin is already lazy
+  version = "^9",
+  lazy = false,
   config = function()
     require("neotest").setup {
       adapters = {
